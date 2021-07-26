2dd8dcff9904e34449e1a30747989c2df5b1a001
daumsimac <daumsimac@gmail.com>
2021-07-26 14:32:24 +0900

    Create nginx-install.sh

---
+++ nginx-install.sh
@@ -0,0 +1,15 @@
+# Install Redis Server
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get install -y $1
+sudo apt-get -y update --fix-missing
+sudo apt-get install -y $1
+
+# Update and then close
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+if netstat -tulpen | grep 80
+then
+exit 0
+fi
